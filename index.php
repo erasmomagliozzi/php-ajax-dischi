df49d7fab4bc8a1a9cd2d0fa98e7b922d98e3dac
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-02-25 17:33:53 +0100

    commit

--- index.php
+++ index.php
@@ -6,14 +6,31 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="dist/app.css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700&display=swap" rel="stylesheet">
   </head>
+  <header>
+    <div class="header">
+      <img src="https://i.kinja-img.com/gawker-media/image/upload/s--ypN6iH9q--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/zrarqnhmkoaihvzss5ek.jpg" alt="logo_spotify">
+    </div>
+  </header>
   <body>
-    <?php foreach ($cd as $key => $descr) {?>
-      <h1><?php echo $descr['titolo'];?></h1>
-      <h2><?php echo $descr['artista'];?></h2>
-      <p><?php echo $descr['anno'];?></p>
-      <img src="<?php echo $descr['img_path'];?>" alt="">
-    <?php } ?>
+    <div class="container_cd">
+      <?php foreach ($cd as $key => $descr) {?>
+        <div class="cd">
+          <img src="<?php echo $descr['img_path'];?>" alt="">
+          <div class="description">
+            <h1><?php echo $descr['titolo'];?></h1>
+            <h2><?php echo $descr['artista'];?></h2>
+            <p><?php echo $descr['anno'];?></p>
+          </div>
+        </div>
+      <?php } ?>
+
+    </div>
 
   </body>
+  <footer>
+
+  </footer>
 </html>
